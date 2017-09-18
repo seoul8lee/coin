50c57a8103ed42c0817d5df237bc60feff6ad4f3
Hyo9 <gywls1743@likelion.org>
2017-09-18 19:21:00 +0900

    id change

--- app/controllers/qna_replies_controller.rb
+++ app/controllers/qna_replies_controller.rb
@@ -7,7 +7,7 @@ class QnaRepliesController < ApplicationController
         @qna_reply.user_id= current_user.id
         @qna_reply.save
        
-        redirect_to "/qnas/show/#{@qna.id}"
+        redirect_to "/qnas/#{@qna.id}"
     end
     
     def todo
@@ -18,16 +18,14 @@ class QnaRepliesController < ApplicationController
     @qna = Qna.find(params[:qna_id])
     @qna_reply = @qna.qna_replies.find(params[:qna_reply_id])
     @qna_reply.destroy
-    redirect_to "/qnas/show/#{@qna.id}"
+    redirect_to "/qnas/#{@qna.id}"
   end
   
   def choose
     @qna = Qna.find(params[:qna_id])
-    @qna.choice=params[:qna_reply_id]
+    @qna.choice=params[:id]
     @qna.save
-    redirect_to "/qnas/show/#{@qna.id}"
-    
-    
+    redirect_to "/qnas/#{@qna.id}"
   end
 
  private
