48e02ab603e6cf387e369932c89030f87858f71e
Hyo9 <gywls1743@likelion.org>
2017-09-18 17:32:31 +0900

    Update routes.rb
    0918.05:32

--- config/routes.rb
+++ config/routes.rb
@@ -1,84 +1,71 @@
 Rails.application.routes.draw do
-  
-  resources :suggests
-  resources :suggests
-  resources :suggests
-  devise_for :users
-  
-  
-  root 'mains#index'
-  get 'mains/index'
-  get 'mains/intro'
-  
-  
-  get 'mypages/index/:user_id' => 'mypages#index'
-  # heeham's
-  post 'qnas/index_c' => 'qnas#ajax_index_c'
-  
-  #
-  get 'qnas' => 'qnas#index'
-  get 'qnas/index'
-  get 'qnas/index_c' 
-  get 'qnas/index_py'
-  get 'qnas/index_java'
-  get 'qnas/index_scratch'
-  get 'qnas/index_html'
-  get 'qnas/index_all' 
-  get 'qnas/index_etc' 
-  get 'qnas/new'
-  get 'qnas/show/:qna_id' => 'qnas#show'
-  post 'qnas/create'
-  post 'qnas/update/:qna_id' => 'qnas#update'
-  get 'qnas/destroy/:qna_id' => 'qnas#destroy'
-  get 'qnas/edit/:qna_id' => 'qnas#edit'
-  
-  get 'qna_replies/create/:qna_id' =>'qna_replies#create'
-  get 'qna_replies/destroy/:qna_id/:qna_reply_id' => 'qna_replies#destroy'
-  get 'qna_replies/new/:qna_id' =>'qna_replies#new'
-  get 'qna_replies/choose/:qna_id/:qna_reply_id' => 'qna_replies#choose'
-  
-  get 'offlines/index'
-  get 'offlines/career'
-  get 'offlines/coding'
-  get 'offlines/career_new'
-  get 'offlines/coding_new'
-  post 'offlines/create'
-  
-  get 'studies/index'
-  get 'studies/index_hot'
-  get 'studies/index_last'
-  get 'studies/new'
-  get 'studies/apply/:study_id'=>'studies#apply'
-   get 'studies/apply_d/:study_id/:apply_id'=>'studies#apply_d'
-  get 'studies/show/:study_id' => 'studies#show'
-  post 'studies/create'
-  post 'studies/update/:study_id' => 'studies#update'
-  get 'studies/destroy/:study_id' => 'studies#destroy'
-  get 'studies/edit/:study_id' => 'studies#edit'
-  
-  get 'mypages/index'
-  
-  
-   resources :qnas do
-    resources :qna_replies
-  end
-
-  
-  
-  resources :reviews do
-      resources :review_replies 
-      collection do
-        get :teacher
-        get :review_career
-        get :review_coding
+    
+    devise_for :users
+    
+    root 'mains#index'
+    
+    get 'mains/index'
+    get 'mains/intro'
+    get 'mypages/index/:user_id' => 'mypages#index'
+#   get 'mypages/index'
+    
+    resources :qnas do
+        resources :qna_replies do
+            member do
+                get 'choose'
+            end
         end
-  end
-  
-  
-  
-#   mount RailsDb::Engine => '/coinkiyom', :as => 'rails_db'
-
-
-  
-  
+        collection do
+            get :index_c
+            get :index_py
+            get :index_java
+            get :index_scratch
+            get :index_html
+            get :index_all
+            get :index_etc
+        end
+    end
+    
+    # heeham's
+    post 'qnas/index_c' => 'qnas#ajax_index_c'
+    
+    
+    get 'offlines/index'
+    get 'offlines/career'
+    get 'offlines/coding'
+    get 'offlines/career_new'
+    get 'offlines/coding_new'
+    post 'offlines/create'
+    
+    
+    resources :reviews do
+        resources :review_replies
+        collection do
+            get :teacher
+            get :review_career
+            get :review_coding
+        end
+    end
+    
+    
+     resources :studies do
+        member do
+            get 'apply'
+          
+        end
+        collection do
+            get :index_hot
+            get :index_last
+        end
+    end
+    
+    get 'studies/:study_id/apply_d/:apply_id'=>'studies#apply_d'
+    
+    resources :suggests
+    
+    
+    #   mount RailsDb::Engine => '/coinkiyom', :as => 'rails_db'
+    
+    
+    
 end
