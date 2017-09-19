c9b3dd3d821592599128fa04fed37389919ba7af
Hyo9 <gywls1743@likelion.org>
2017-09-19 19:31:12 +0900

    Update qna_replies_controller.rb
    0919.07:31

--- app/controllers/qna_replies_controller.rb
+++ app/controllers/qna_replies_controller.rb
@@ -3,8 +3,10 @@ class QnaRepliesController < ApplicationController
     def create
         @qna = Qna.find(params[:qna_id])
         @qna_reply = @qna.qna_replies.create(reply_params)
+    if current_user.nil?     
         @qna_reply.qna_reply_user = current_user.nickname
         @qna_reply.user_id= current_user.id
+    ends    
         @qna_reply.save
        
         redirect_to "/qnas/#{@qna.id}"
