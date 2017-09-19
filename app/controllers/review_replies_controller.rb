273ccee080358557002908ec14b62140dc23e5e2
Hyo9 <gywls1743@likelion.org>
2017-09-19 19:32:05 +0900

    Update review_replies_controller.rb
    0919.07:32

--- app/controllers/review_replies_controller.rb
+++ app/controllers/review_replies_controller.rb
@@ -3,7 +3,9 @@ class ReviewRepliesController < ApplicationController
         @review = Review.find(params[:review_id])
         @review_reply = @review.review_replies.build
         @review_reply.body = params[:body]
+    if current_user.nil?     
         @review_reply.user = current_user
+    end    
         @review_reply.save
         redirect_to "/reviews/#{@review.id}"
     end
